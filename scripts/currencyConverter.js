611fc8b23f011bea7ab3658f8fb6151ebe8bf4ab
Dahian Sosa <shaco-dark@outlook.com>
2022-12-06 13:21:11 -0300

    Style+Bug Fix

--- scripts/currencyConverter.js
+++ scripts/currencyConverter.js
@@ -3,8 +3,7 @@ const currencyAmount = document.querySelector("#currencyAmount");
 const currencyFrom = document.querySelector("#currencyFrom");
 const currencyTo = document.querySelector("#currencyTo");
 const currencyResult = document.querySelector("#currencyResult");
-const FromSelectCurrency = currencyFrom.options[currencyFrom.value].textContent
-const FromToCurrency = currencyTo.options[currencyTo.value].textContent
+
 
 
 eventListeners ();
@@ -45,7 +44,9 @@ function validConverter () {
 //!Submit converter Btn after validation
 function submitConverterBtn (){
 
-	const url = `https://alpha-vantage.p.rapidapi.com/query?from_currency=${FromSelectCurrency}&function=CURRENCY_EXCHANGE_RATE&to_currency=${FromToCurrency}`
+	let FromSelectCurrency = currencyFrom.options[currencyFrom.value].textContent
+	let FromToCurrency = currencyTo.options[currencyTo.value].textContent
+	let url = `https://alpha-vantage.p.rapidapi.com/query?from_currency=${FromSelectCurrency}&function=CURRENCY_EXCHANGE_RATE&to_currency=${FromToCurrency}`
 	const options = {
 		method: 'GET',
 		headers: {
@@ -67,11 +68,14 @@ function currencyConverter (data) {
 
 	console.log("Desde function currencyConverter", data);
 	let currencyAmountRate = currencyAmount.valueAsNumber
-	const FromSelectCurrency = currencyFrom.options[currencyFrom.value].textContent
-	const FromToCurrency = currencyTo.options[currencyTo.value].textContent
-	const currencyConverted = Number(Object.values(data)[4])*currencyAmount.valueAsNumber; 
+	let FromSelectCurrency = currencyFrom.options[currencyFrom.value].textContent
+	let FromToCurrency = currencyTo.options[currencyTo.value].textContent
+	let currencyConverted = Number(Object.values(data)[4])*currencyAmount.valueAsNumber; 
 	
 	let resultContent = `Ingreso de Dinero: ${currencyAmount.valueAsNumber} ${FromSelectCurrency}. Egreso de Dinero ${currencyConverted} ${FromToCurrency}`
+
+
+	console.log(Number(Object.values(data)[4]));
 
 	currencyResult.innerHTML = `
 		<div class="lds-dual-ring"></div>
